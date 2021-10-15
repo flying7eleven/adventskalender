9c0636cf7c7b6eaf31e2fb1212c7810fb1cb5869
Tim Janke <tim+github@janke.biz>
2021-10-15 15:58:30 +0200

    :construction: Add the rust type for the new database table

---
+++ adventskalender-backend/src/lib.rs
@@ -0,0 +1,20 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate diesel;
+#[cfg(not(debug_assertions))]
+#[macro_use]
+extern crate diesel_migrations;
+
+use chrono::NaiveDate;
+use schema::participants;
+
+mod schema;
+
+#[derive(Queryable, Identifiable)]
+pub struct Participant {
+    pub id: i32,
+    pub first_name: String,
+    pub last_name: String,
+    pub won_on: NaiveDate,
+}
