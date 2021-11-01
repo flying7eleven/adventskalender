650a2d3e14c2ea1ffe6c8ee834a534e92d55aa27
Tim Janke <tim+github@janke.biz>
2021-11-01 13:58:10 +0100

    :sparkles: Update the layout of the stats cards

--- adventskalender-frontend/src/index.tsx
+++ adventskalender-frontend/src/index.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import CssBaseline from '@mui/material/CssBaseline';
 import { AuthenticatedView } from './views/AuthenticatedView';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
 
 ReactDOM.render(
     <React.StrictMode>
-        <CssBaseline />
-        <AuthenticatedView />
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <AuthenticatedView />
+        </ThemeProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
--- adventskalender-frontend/src/views/AuthenticatedView/index.tsx
+++ adventskalender-frontend/src/views/AuthenticatedView/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { ParticipantCount, API_BACKEND_URL } from '../../api';
 import { OutlinedCard } from '../../components/OutlinedCard';
 import { TopControlBar } from '../../components/TopControlBar';
+import Grid from '@mui/material/Grid';
 
 export const AuthenticatedView = () => {
     const [participantCount, setParticipantCount] = useState<ParticipantCount>({ number_of_participants: 0, number_of_participants_won: 0, number_of_participants_still_in_raffle: 0 });
@@ -16,10 +17,20 @@ export const AuthenticatedView = () => {
 
     return (
         <>
-            <TopControlBar title={'Adventskalender'} actionTitle={'Logout'} actionHandler={() => {}} />
-            <OutlinedCard headline={'Overall participants'} value={`${participantCount.number_of_participants}`} description={'people are in the raffle'} />
-            <OutlinedCard headline={'Participants who won'} value={`${participantCount.number_of_participants_won}`} description={'people already won in the raffle'} />
-            <OutlinedCard headline={'Participants still eligible'} value={`${participantCount.number_of_participants_still_in_raffle}`} description={'people are still able to win'} />
+            <Grid container columns={12} spacing={2}>
+                <Grid item xs={12}>
+                    <TopControlBar title={'Adventskalender'} actionTitle={'Logout'} actionHandler={() => {}} />
+                </Grid>
+                <Grid item>
+                    <OutlinedCard headline={'Overall participants'} value={`${participantCount.number_of_participants}`} description={'people are in the raffle'} />
+                </Grid>
+                <Grid item>
+                    <OutlinedCard headline={'Participants who won'} value={`${participantCount.number_of_participants_won}`} description={'people already won in the raffle'} />
+                </Grid>
+                <Grid item>
+                    <OutlinedCard headline={'Participants still eligible'} value={`${participantCount.number_of_participants_still_in_raffle}`} description={'people are still able to win'} />
+                </Grid>
+            </Grid>
         </>
     );
 };
