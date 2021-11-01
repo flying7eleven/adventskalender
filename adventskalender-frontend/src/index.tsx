633b81b33713abac3386337f38168ab6d952d1f1
Tim Janke <tim+github@janke.biz>
2021-11-01 13:27:33 +0100

    :sparkles: Add a component for showing the stats

--- adventskalender-frontend/src/index.tsx
+++ adventskalender-frontend/src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import CssBaseline from '@mui/material/CssBaseline';
 import { AuthenticatedView } from './views/AuthenticatedView';
 
 ReactDOM.render(
     <React.StrictMode>
+        <CssBaseline />
         <AuthenticatedView />
     </React.StrictMode>,
     document.getElementById('root')
--- adventskalender-frontend/src/views/AuthenticatedView/index.tsx
+++ adventskalender-frontend/src/views/AuthenticatedView/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { ParticipantCount, API_BACKEND_URL } from '../../api';
+import { OutlinedCard } from '../../components/OutlinedCard';
 
 export const AuthenticatedView = () => {
     const [participantCount, setParticipantCount] = useState<ParticipantCount>({ number_of_participants: 0, number_of_participants_won: 0, number_of_participants_still_in_raffle: 0 });
@@ -13,10 +14,10 @@ export const AuthenticatedView = () => {
     }, []);
 
     return (
-        <div>
-            <div>{participantCount.number_of_participants_still_in_raffle}</div>
-            <div>{participantCount.number_of_participants}</div>
-            <div>{participantCount.number_of_participants_won}</div>
-        </div>
+        <>
+            <OutlinedCard headline={'Overall participants'} value={`${participantCount.number_of_participants}`} description={'people are in the raffle'} />
+            <OutlinedCard headline={'Participants who won'} value={`${participantCount.number_of_participants_won}`} description={'people already won in the raffle'} />
+            <OutlinedCard headline={'Participants still eligible'} value={`${participantCount.number_of_participants_still_in_raffle}`} description={'people are still able to win'} />
+        </>
     );
 };
