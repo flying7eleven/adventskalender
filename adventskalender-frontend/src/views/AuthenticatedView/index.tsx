4e881f9b66a90d6ef04b23fa46751de86d42d207
Tim Janke <tim+github@janke.biz>
2021-11-01 13:35:58 +0100

    :sparkles: Add a top navigation bar to the frontend

--- adventskalender-frontend/src/views/AuthenticatedView/index.tsx
+++ adventskalender-frontend/src/views/AuthenticatedView/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { ParticipantCount, API_BACKEND_URL } from '../../api';
 import { OutlinedCard } from '../../components/OutlinedCard';
+import { TopControlBar } from '../../components/TopControlBar';
 
 export const AuthenticatedView = () => {
     const [participantCount, setParticipantCount] = useState<ParticipantCount>({ number_of_participants: 0, number_of_participants_won: 0, number_of_participants_still_in_raffle: 0 });
@@ -15,6 +16,7 @@ export const AuthenticatedView = () => {
 
     return (
         <>
+            <TopControlBar title={'Adventskalender'} actionTitle={'Logout'} actionHandler={() => {}} />
             <OutlinedCard headline={'Overall participants'} value={`${participantCount.number_of_participants}`} description={'people are in the raffle'} />
             <OutlinedCard headline={'Participants who won'} value={`${participantCount.number_of_participants_won}`} description={'people already won in the raffle'} />
             <OutlinedCard headline={'Participants still eligible'} value={`${participantCount.number_of_participants_still_in_raffle}`} description={'people are still able to win'} />
