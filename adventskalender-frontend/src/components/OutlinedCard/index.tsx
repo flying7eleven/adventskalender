a5569520fd917d492e998d4281a8a68ed44f819e
Tim Janke <tim+github@janke.biz>
2021-11-01 13:37:44 +0100

    :fire: Remove the action button from the stats cards

--- adventskalender-frontend/src/components/OutlinedCard/index.tsx
+++ adventskalender-frontend/src/components/OutlinedCard/index.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 interface Props {
@@ -28,9 +26,6 @@ export const OutlinedCard = (props: Props) => {
                         {props.description}
                     </Typography>
                 </CardContent>
-                <CardActions>
-                    <Button size="small">Learn More</Button>
-                </CardActions>
             </Card>
         </Box>
     );
