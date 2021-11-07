3a93a528d971f32edacfb50c460c69e86eb68442
Tim Janke <tim+github@janke.biz>
2021-11-07 16:18:43 +0100

    :white_check_mark: Add a new test for the frontend

---
+++ adventskalender-frontend/src/components/OutlinedCard/index.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { create } from 'react-test-renderer';
+import { OutlinedCard } from "./index";
+
+describe('OutlinedCard component', () => {
+    test('Matches the snapshot', () => {
+        const card = create(<OutlinedCard headline={'Headline'} value={'Value'} description={'Description'} />);
+        expect(card.toJSON()).toMatchSnapshot();
+    });
+});
