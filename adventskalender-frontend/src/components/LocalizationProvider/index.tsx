26d369a7417a10f4c7ea8dc2689ae993138a2aaf
Tim Janke <tim+github@janke.biz>
2021-11-07 15:07:21 +0100

    :recycle: Move a lot of the translation code to the LocalizationProvider

--- adventskalender-frontend/src/components/LocalizationProvider/index.tsx
+++ adventskalender-frontend/src/components/LocalizationProvider/index.tsx
@@ -12,11 +12,52 @@ interface Props {
 
 interface LocalizationContextType {
     resources: ResourceDefinitions;
+    translate: (key: string) => string;
 }
 
 export const LocalizationContext = createContext<LocalizationContextType>(null!);
 
 export const LocalizationProvider = (props: Props) => {
-    const value = { resources: props.resources };
+    const deepFind = (obj: any, path: string) => {
+        const paths = path.split('.');
+        let current = obj;
+        for (let i = 0; i < paths.length; ++i) {
+            if (current[paths[i]] === undefined) {
+                return undefined;
+            } else {
+                current = current[paths[i]];
+            }
+        }
+        return current;
+    };
+
+    const getTranslationLanguage = () => {
+        const navigatorLanguage = navigator.language.split('-');
+        switch (navigatorLanguage[0].toLowerCase()) {
+            case 'de':
+                return props.resources.german;
+            case 'en':
+            default:
+                return props.resources.english;
+        }
+    };
+
+    const getTranslatedString = (key: string) => {
+        // get the correct dictionary for the translation
+        const translationLanguage = getTranslationLanguage();
+
+        // get the translation for the key and handle if we did not find it
+        const translatedValue = deepFind(translationLanguage, key);
+        if (!translatedValue) {
+            console.error(`Could not find localization for '${key}' (selected navigator language was ${navigator.language})`);
+            return key;
+        }
+
+        // return the translated value
+        return translatedValue;
+    };
+
+    const value = { resources: props.resources, translate: getTranslatedString };
+
     return <LocalizationContext.Provider value={value}>{props.children}</LocalizationContext.Provider>;
 };
--- adventskalender-frontend/src/components/Localized/index.tsx
+++ adventskalender-frontend/src/components/Localized/index.tsx
@@ -6,47 +6,6 @@ interface Props {
 }
 
 export const Localized = (props: Props) => {
-    // eslint-disable-next-line
     const localizationContext = useContext(LocalizationContext);
-
-    const deepFind = (obj: any, path: string) => {
-        const paths = path.split('.');
-        let current = obj;
-        for (let i = 0; i < paths.length; ++i) {
-            if (current[paths[i]] === undefined) {
-                return undefined;
-            } else {
-                current = current[paths[i]];
-            }
-        }
-        return current;
-    };
-
-    const getTranslationLanguage = () => {
-        const navigatorLanguage = navigator.language.split('-');
-        switch (navigatorLanguage[0].toLowerCase()) {
-            case 'de':
-                return localizationContext.resources.german;
-            case 'en':
-            default:
-                return localizationContext.resources.english;
-        }
-    };
-
-    const getTranslationForKeyParts = (key: string) => {
-        // get the correct dictionary for the translation
-        const translationLanguage = getTranslationLanguage();
-
-        // get the translation for the key and handle if we did not find it
-        const translatedValue = deepFind(translationLanguage, key);
-        if (!translatedValue) {
-            console.error(`Could not find localization for '${key}' (selected navigator language was ${navigator.language})`);
-            return key;
-        }
-
-        // return the translated value
-        return translatedValue;
-    };
-
-    return <>{getTranslationForKeyParts(props.translationKey)}</>;
+    return <>{localizationContext.translate(props.translationKey)}</>;
 };
