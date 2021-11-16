29b01eea62962fd79e827a695d6e93ff3f083522
Tim Janke <tim+github@janke.biz>
2021-11-16 16:34:56 +0100

    :recycle: Change the layout for the calendar view

--- adventskalender-frontend/src/components/WinnerCard/index.tsx
+++ adventskalender-frontend/src/components/WinnerCard/index.tsx
@@ -1,24 +1,21 @@
 import { Localized } from '../Localized';
-import { Avatar, Button, Card, CardContent, Divider, Stack, Typography } from '@mui/material';
+import { Box, Button, Card, CardContent, Divider, Stack, Typography } from '@mui/material';
 
 interface Props {
     winningDate: string;
     listOfWinner: SingleWinnerInformation[];
 }
 
-const PersonItem = ({ src, name }: { src: string; name: string }) => {
+const PersonItem = ({ name }: { name: string }) => {
     return (
-        <Stack direction={'column'}>
-            <Stack direction={'row'} spacing={1}>
-                <Avatar src={src} />
-                <Stack direction={'column'} spacing={1}>
-                    <Typography>{name}</Typography>
-                    <Button variant={'outlined'} disabled>
-                        <Localized translationKey={'calendar.cards.winners.button_remove'} />
-                    </Button>
-                </Stack>
-            </Stack>
-        </Stack>
+        <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
+            <Typography variant={'body1'} sx={{ textAlign: 'left' }}>
+                {name}
+            </Typography>
+            <Button variant={'outlined'} sx={{ borderRadius: '20px', fontSize: 'x-small', textAlign: 'right' }} disabled>
+                <Localized translationKey={'calendar.cards.winners.button_remove'} />
+            </Button>
+        </Box>
     );
 };
 
@@ -30,7 +27,6 @@ export const WinnerCard = (props: Props) => {
                 <PersonItem
                     key={`person-item-${props.listOfWinner[i].first_name.toLowerCase()}-${props.listOfWinner[i].last_name.toLocaleLowerCase()}`}
                     name={`${props.listOfWinner[i].first_name} ${props.listOfWinner[i].last_name}`}
-                    src={'images/christmasTree.jpg'}
                 />
             );
             if (i !== props.listOfWinner.length - 1) {
@@ -40,14 +36,28 @@ export const WinnerCard = (props: Props) => {
         return elements;
     };
 
+    const getFormattedDate = (inputDate: string) => {
+        const dateRegex = /(?<year>\d{4})-(?<month>\d{1,2})-(?<day>\d{1,2})/g;
+        const matchedString = dateRegex.exec(inputDate);
+        if (matchedString) {
+            return `${matchedString.groups?.['day']}.${matchedString.groups?.['month']}.${matchedString.groups?.['year']}`;
+        }
+        return inputDate;
+    };
+
     return (
         <>
             <Card variant="outlined">
                 <CardContent>
                     <Stack direction={'column'} spacing={1}>
-                        <Typography component={'h1'}>
-                            <Localized translationKey={'calendar.cards.winners.headline'} placeholder={props.winningDate} />
-                        </Typography>
+                        <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
+                            <Typography variant={'subtitle1'} sx={{ fontWeight: 'bold', textAlign: 'left' }}>
+                                <Localized translationKey={'calendar.cards.winners.headline'} />
+                            </Typography>
+                            <Typography variant={'subtitle1'} sx={{ fontWeight: 'bold', textAlign: 'right' }}>
+                                {getFormattedDate(props.winningDate)}
+                            </Typography>
+                        </Box>
                         {getWinningEntries()}
                     </Stack>
                 </CardContent>
