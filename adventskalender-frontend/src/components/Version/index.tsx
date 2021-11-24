a110561706b1836ee8fc5d75006b572278798eb5
Tim Janke <tim+github@janke.biz>
2021-11-24 13:48:50 +0100

    :sparkles: Add a link to the soruce code to the version page

--- adventskalender-frontend/src/components/Version/index.tsx
+++ adventskalender-frontend/src/components/Version/index.tsx
@@ -2,7 +2,7 @@ import Grid from '@mui/material/Grid';
 import { useEffect, useState } from 'react';
 import { API_BACKEND_URL, VersionInformation } from '../../api';
 import packageJson from '../../../package.json';
-import { Box, Button, Card, CardContent, Divider, Stack, Typography } from '@mui/material';
+import { Box, Card, CardContent, Divider, Link, Stack, Typography } from '@mui/material';
 import { Localized } from '../Localized';
 
 export const Version = () => {
@@ -32,46 +32,56 @@ export const Version = () => {
     }, []);
 
     return (
-        <Grid container columns={12} spacing={2} justifyContent={'center'} alignItems={'center'}>
-            <Grid item xs={3}>
-                <Card variant="outlined">
-                    <CardContent>
-                        <Stack direction={'column'} spacing={1}>
-                            <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
-                                <Typography variant={'subtitle1'} sx={{ fontWeight: 'bold', textAlign: 'left' }}>
-                                    <Localized translationKey={'version.card.headline'} />
-                                </Typography>
-                            </Box>
-                            <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
-                                <Typography variant={'body1'} sx={{ textAlign: 'left' }}>
-                                    <Localized translationKey={'version.card.categories.frontend_version'} />
-                                </Typography>
-                                <Typography variant={'body1'} sx={{ textAlign: 'right' }}>
-                                    {packageJson.version}
-                                </Typography>
-                            </Box>
-                            <Divider />
-                            <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
-                                <Typography variant={'body1'} sx={{ textAlign: 'left' }}>
-                                    <Localized translationKey={'version.card.categories.backend_version'} />
-                                </Typography>
-                                <Typography variant={'body1'} sx={{ textAlign: 'right' }}>
-                                    {backendVersionInformation.backend_version}
-                                </Typography>
-                            </Box>
-                            <Divider />
-                            <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
-                                <Typography variant={'body1'} sx={{ textAlign: 'left' }}>
-                                    <Localized translationKey={'version.card.categories.backend_rustc_version'} />
-                                </Typography>
-                                <Typography variant={'body1'} sx={{ textAlign: 'right' }}>
-                                    {backendVersionInformation.rustc_version}
-                                </Typography>
-                            </Box>
-                        </Stack>
-                    </CardContent>
-                </Card>
+        <Stack spacing={2}>
+            <Grid container columns={12} spacing={2} justifyContent={'center'} alignItems={'center'}>
+                <Grid item xs={3}>
+                    <Card variant="outlined">
+                        <CardContent>
+                            <Stack direction={'column'} spacing={1}>
+                                <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
+                                    <Typography variant={'subtitle1'} sx={{ fontWeight: 'bold', textAlign: 'left' }}>
+                                        <Localized translationKey={'version.card.headline'} />
+                                    </Typography>
+                                </Box>
+                                <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
+                                    <Typography variant={'body1'} sx={{ textAlign: 'left' }}>
+                                        <Localized translationKey={'version.card.categories.frontend_version'} />
+                                    </Typography>
+                                    <Typography variant={'body1'} sx={{ textAlign: 'right' }}>
+                                        {packageJson.version}
+                                    </Typography>
+                                </Box>
+                                <Divider />
+                                <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
+                                    <Typography variant={'body1'} sx={{ textAlign: 'left' }}>
+                                        <Localized translationKey={'version.card.categories.backend_version'} />
+                                    </Typography>
+                                    <Typography variant={'body1'} sx={{ textAlign: 'right' }}>
+                                        {backendVersionInformation.backend_version}
+                                    </Typography>
+                                </Box>
+                                <Divider />
+                                <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
+                                    <Typography variant={'body1'} sx={{ textAlign: 'left' }}>
+                                        <Localized translationKey={'version.card.categories.backend_rustc_version'} />
+                                    </Typography>
+                                    <Typography variant={'body1'} sx={{ textAlign: 'right' }}>
+                                        {backendVersionInformation.rustc_version}
+                                    </Typography>
+                                </Box>
+                            </Stack>
+                        </CardContent>
+                    </Card>
+                </Grid>
             </Grid>
-        </Grid>
+            <Typography variant={'subtitle2'} sx={{ textAlign: 'center' }}>
+                <span dangerouslySetInnerHTML={{ __html: 'Crafted with &#10084; in Düsseldorf.&nbsp;' }} />
+                Get the source code at{' '}
+                <Link href={'https://github.com/flying7eleven/adventskalender'} target={'_blank'}>
+                    GitHub
+                </Link>
+                .
+            </Typography>
+        </Stack>
     );
 };
