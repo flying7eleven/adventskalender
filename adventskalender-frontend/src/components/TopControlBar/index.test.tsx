98c8ac2e33bdbece345a4c3d88fcb1433cdd7d56
Tim Janke <tim+github@janke.biz>
2021-11-24 11:05:48 +0100

    :lipstick: Improve the UX of the side navigation

--- adventskalender-frontend/src/components/TopControlBar/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { create } from 'react-test-renderer';
-import { TopControlBar } from './index';
-
-describe('TopControlBar component', () => {
-    test('Matches the snapshot', () => {
-        const button = create(<TopControlBar title={'Title'} actionTitle={'Action Title'} />);
-        expect(button.toJSON()).toMatchSnapshot();
-    });
-});
