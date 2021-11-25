a8538d141bceda70a653d5c0f15c5172d5e3f2a1
Tim Janke <tim+github@janke.biz>
2021-11-25 11:35:44 +0100

    :construction: Add the required controls to the settings page

--- adventskalender-frontend/src/components/Settings/index.tsx
+++ adventskalender-frontend/src/components/Settings/index.tsx
@@ -1,3 +1,42 @@
+import { Button, Card, CardContent, Stack, TextField, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import { Localized } from '../Localized';
+import { useContext } from 'react';
+import { LocalizationContext } from '../LocalizationProvider';
+
 export const Settings = () => {
-    return <div>No content</div>;
+    const localizationContext = useContext(LocalizationContext);
+
+    return (
+        <Grid container columns={12} spacing={2} justifyContent={'center'} alignItems={'center'}>
+            <Grid item xs={3}>
+                <Card variant="outlined">
+                    <CardContent>
+                        <Stack direction={'column'} spacing={1}>
+                            <Typography variant={'subtitle1'} sx={{ fontWeight: 'bold', textAlign: 'left' }}>
+                                <Localized translationKey={'settings.cards.password.headline'} />
+                            </Typography>
+                            <TextField
+                                id={'first-password-input'}
+                                label={localizationContext.translate('settings.cards.password.labels.password_first')}
+                                type={'password'}
+                                autoComplete={'new-password'}
+                                variant={'outlined'}
+                            />
+                            <TextField
+                                id={'first-password-input'}
+                                label={localizationContext.translate('settings.cards.password.labels.password_repeated')}
+                                type={'password'}
+                                autoComplete={'new-password'}
+                                variant={'outlined'}
+                            />
+                            <Button variant="contained" disableElevation>
+                                <Localized translationKey={'settings.cards.password.buttons.change'} />
+                            </Button>
+                        </Stack>
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
+    );
 };
