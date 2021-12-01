536050abc469c0dff115690c9fc35056b5a21921
Tim Janke <tim+github@janke.biz>
2021-12-01 14:55:04 +0100

    :adhesive_bandage: Correct the layout of the dashboard on smaller screens

--- adventskalender-frontend/src/views/DashboardView/index.tsx
+++ adventskalender-frontend/src/views/DashboardView/index.tsx
@@ -317,21 +317,21 @@ export const DashboardView = () => {
                 </DialogActions>
             </Dialog>
             <Grid container columns={12} spacing={2} justifyContent={'center'} alignItems={'center'}>
-                <Grid item xs={2}>
+                <Grid item>
                     <OutlinedCard
                         headline={localizationContext.translate('dashboard.cards.total.title')}
                         value={`${participantCount.number_of_participants}`}
                         description={localizationContext.translate('dashboard.cards.total.description')}
                     />
                 </Grid>
-                <Grid item xs={2}>
+                <Grid item>
                     <OutlinedCard
                         headline={localizationContext.translate('dashboard.cards.won.title')}
                         value={`${participantCount.number_of_participants_won}`}
                         description={localizationContext.translate('dashboard.cards.won.description')}
                     />
                 </Grid>
-                <Grid item xs={2}>
+                <Grid item>
                     <OutlinedCard
                         headline={localizationContext.translate('dashboard.cards.eligible.title')}
                         value={`${participantCount.number_of_participants_still_in_raffle}`}
