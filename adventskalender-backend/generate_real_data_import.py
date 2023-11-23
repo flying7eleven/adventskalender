8ad0c7baef2d4db9e61852992672f3096714b622
Tim Janke <tim+github@janke.biz>
2023-11-23 06:32:48 +0100

    :hammer: Switch the order of the first and last name for the participants

--- adventskalender-backend/generate_real_data_import.py
+++ adventskalender-backend/generate_real_data_import.py
@@ -8,7 +8,7 @@ def main():
         csv_reader = reader(input_file_handle, delimiter=';')
         with open(output_file, mode='w+') as output_file_handle:
             for current_data_row in csv_reader:
-                sql_line = f'INSERT INTO participants(first_name, last_name) VALUES ( \'{current_data_row[1].strip()}\', \'{current_data_row[0].strip()}\' );\n'
+                sql_line = f'INSERT INTO participants(first_name, last_name) VALUES ( \'{current_data_row[0].strip()}\', \'{current_data_row[1].strip()}\' );\n'
                 output_file_handle.write(sql_line)
 
 
