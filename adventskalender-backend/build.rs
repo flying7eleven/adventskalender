b452f93882bc2c5880076f2868758a11a00f510b
Tim Janke <tim+github@janke.biz>
2024-09-23 10:03:26 +0200

    :arrow_up: Upgrade all backend dependencies

--- adventskalender-backend/build.rs
+++ adventskalender-backend/build.rs
@@ -1,4 +1,5 @@
-use vergen::EmitBuilder;
+use vergen::{BuildBuilder, CargoBuilder, Emitter, RustcBuilder};
+use vergen_git2::Git2Builder;
 
 fn main() {
     // set up the configuration for vergen
@@ -6,13 +7,22 @@ fn main() {
     //*config.build_mut().kind_mut() = TimestampKind::DateAndTime;
     //*config.build_mut().timezone_mut() = TimeZone::Utc;
 
+    let build = BuildBuilder::all_build().expect("Failed to build info emitter");
+    let cargo = CargoBuilder::all_cargo().expect("Failed to build cargo vergen emitter");
+    let git2 = Git2Builder::all_git().expect("Failed to build git vergen emitter");
+    let rustc = RustcBuilder::all_rustc().expect("Failed to build rustc vergen emitter");
+
     // configure vergen to generate the required environment variables
-    if let Err(error) = EmitBuilder::builder()
+    if let Err(error) = Emitter::default()
+        .add_instructions(&build)
+        .expect("Failed to create emit emitter for: build")
+        .add_instructions(&cargo)
+        .expect("Failed to create emit emitter for: cargo")
+        .add_instructions(&git2)
+        .expect("Failed to create emit emitter for: git")
+        .add_instructions(&rustc)
+        .expect("Failed to create emit emitter for: rustc")
         .fail_on_error()
-        .all_build()
-        .all_cargo()
-        .all_rustc()
-        .git_describe(false, true, None)
         .emit()
     {
         panic!(
