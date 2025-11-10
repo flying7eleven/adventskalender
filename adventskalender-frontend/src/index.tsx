97047352535ef0cbb6d842ba3e0db65378d16f98
Tim Janke <tim+github@janke.biz>
2025-11-10 08:06:32 +0100

    :bug: (frontend): Correct some warnings

--- adventskalender-frontend/src/index.tsx
+++ adventskalender-frontend/src/index.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from './components/App';
 
 // determine the users locale and create a theme we want to use in the whole app
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+    throw new Error('Failed to find the root element');
+}
+
+const root = createRoot(rootElement);
+root.render(
     <StrictMode>
         <App />
-    </StrictMode>,
-    document.getElementById('root')
+    </StrictMode>
 );
