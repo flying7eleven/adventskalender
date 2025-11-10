dc1f8bd1445dee3713cf98320dac8a2fdf1693a5
Tim Janke <tim+github@janke.biz>
2025-11-10 13:51:08 +0100

    :recycle: (frontend): Replace MUI navigation with custom components

--- adventskalender-frontend/src/views/AuthenticatedView/index.tsx
+++ adventskalender-frontend/src/views/AuthenticatedView/index.tsx
@@ -1,97 +1,19 @@
 import { useState, useContext, ReactElement, useEffect } from 'react';
 import { useAuthentication } from '../../hooks/useAuthentication';
 import { useLocation, useNavigate } from 'react-router-dom';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import { Box, Divider, IconButton, Stack, Toolbar, Typography } from '@mui/material';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-import MuiDrawer from '@mui/material/Drawer';
-import { styled, Theme, CSSObject } from '@mui/material/styles';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import ListItemText from '@mui/material/ListItemText';
-import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
-import MenuIcon from '@mui/icons-material/Menu';
-import { Button } from '@/components/ui/button';
+import { LayoutDashboard, Calendar, Settings, Info } from 'lucide-react';
 import { LocalizedText } from '../../components/LocalizedText';
-import InfoIcon from '@mui/icons-material/Info';
-import SettingsIcon from '@mui/icons-material/Settings';
 import { LocalizationContext } from '../../provider/LocalizationContext';
 import { sessionManager } from '../../utils/SessionManager';
 import { SessionExpiryWarningDialog } from '../../dialogs/SessionExpiryWarningDialog';
 import { API_BACKEND_URL } from '../../api';
+import { NavigationDrawer } from '../../components/NavigationDrawer';
+import { AppHeader } from '../../components/AppHeader';
+import { cn } from '../../lib/utils';
 
 interface Props {
     content: ReactElement;
 }
-
-const drawerWidth = 300;
-
-const openedMixin = (theme: Theme): CSSObject => ({
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-    }),
-    overflowX: 'hidden',
-});
-
-const closedMixin = (theme: Theme): CSSObject => ({
-    transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: `calc(${theme.spacing(7)} + 1px)`,
-});
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-}));
-
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(({ theme, open }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    ...(open && {
-        ...openedMixin(theme),
-        '& .MuiDrawer-paper': openedMixin(theme),
-    }),
-    ...(!open && {
-        ...closedMixin(theme),
-        '& .MuiDrawer-paper': closedMixin(theme),
-    }),
-}));
-
-interface AppBarProps extends MuiAppBarProps {
-    open?: boolean;
-}
-
-const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== 'open',
-})<AppBarProps>(({ theme, open }) => ({
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    ...(open && {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    }),
-}));
 
 export const AuthenticatedView = (props: Props) => {
     const [navigationDrawerOpen, setNavigationDrawerOpen] = useState<boolean>(false);
@@ -185,71 +107,68 @@ export const AuthenticatedView = (props: Props) => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [auth.isAuthenticated]);
 
+    // Define navigation items
+    const mainNavItems = [
+        {
+            key: 'Dashboard',
+            icon: <LayoutDashboard className="h-5 w-5" />,
+            label: <LocalizedText translationKey="global.navigation.dashboard" />,
+            onClick: onClickOnDashboard,
+            selected: isSelected('/'),
+        },
+        {
+            key: 'Calendar',
+            icon: <Calendar className="h-5 w-5" />,
+            label: <LocalizedText translationKey="global.navigation.calendar" />,
+            onClick: onClickOnCalendar,
+            selected: isSelected('/calendar'),
+        },
+    ];
+
+    const settingsNavItems = [
+        {
+            key: 'Settings',
+            icon: <Settings className="h-5 w-5" />,
+            label: <LocalizedText translationKey="global.navigation.settings" />,
+            onClick: onClickSettings,
+            selected: isSelected('/settings'),
+        },
+        {
+            key: 'Version',
+            icon: <Info className="h-5 w-5" />,
+            label: <LocalizedText translationKey="global.navigation.version" />,
+            onClick: onClickVersion,
+            selected: isSelected('/version'),
+        },
+    ];
+
     return (
         <>
             <SessionExpiryWarningDialog isOpen={showExpiryWarning} onExtendSession={handleExtendSession} onLogout={logoutUser} />
-            <Stack>
-                <AppBar position="fixed" open={navigationDrawerOpen}>
-                    <Toolbar>
-                        <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={toggleDrawer}>
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                            <LocalizedText translationKey={'dashboard.navigation.app_title'} />
-                        </Typography>
-                        <Button variant="ghost" onClick={logoutUser} className="text-inherit">
-                            <LocalizedText translationKey={'dashboard.navigation.logout_button'} />
-                        </Button>
-                    </Toolbar>
-                </AppBar>
-                <Drawer variant="permanent" open={navigationDrawerOpen}>
-                    <DrawerHeader>
-                        <IconButton onClick={toggleDrawer}>
-                            <ChevronLeftIcon />
-                        </IconButton>
-                    </DrawerHeader>
-                    <Divider />
-                    <List>
-                        <ListItem disablePadding>
-                            <ListItemButton key={'Dashboard'} onClick={onClickOnDashboard} selected={isSelected('/')}>
-                                <ListItemIcon>
-                                    <DashboardIcon />
-                                </ListItemIcon>
-                                <ListItemText primary={localizationContext.translate('global.navigation.dashboard')} />
-                            </ListItemButton>
-                        </ListItem>
-                        <ListItem disablePadding>
-                            <ListItemButton key={'Calendar'} onClick={onClickOnCalendar} selected={isSelected('/calendar')}>
-                                <ListItemIcon>
-                                    <CalendarTodayIcon />
-                                </ListItemIcon>
-                                <ListItemText primary={localizationContext.translate('global.navigation.calendar')} />
-                            </ListItemButton>
-                        </ListItem>
-                        <Divider />
-                        <ListItem disablePadding>
-                            <ListItemButton key={'Settings'} onClick={onClickSettings} selected={isSelected('/settings')}>
-                                <ListItemIcon>
-                                    <SettingsIcon />
-                                </ListItemIcon>
-                                <ListItemText primary={localizationContext.translate('global.navigation.settings')} />
-                            </ListItemButton>
-                        </ListItem>
-                        <ListItem disablePadding>
-                            <ListItemButton key={'Version'} onClick={onClickVersion} selected={isSelected('/version')}>
-                                <ListItemIcon>
-                                    <InfoIcon />
-                                </ListItemIcon>
-                                <ListItemText primary={localizationContext.translate('global.navigation.version')} />
-                            </ListItemButton>
-                        </ListItem>
-                    </List>
-                </Drawer>
-                <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-                    <DrawerHeader />
-                    {props.content}
-                </Box>
-            </Stack>
+
+            <NavigationDrawer open={navigationDrawerOpen} onToggle={toggleDrawer} items={mainNavItems} settingsItems={settingsNavItems} />
+
+            <AppHeader
+                open={navigationDrawerOpen}
+                onMenuClick={toggleDrawer}
+                onLogout={logoutUser}
+                title={<LocalizedText translationKey={'dashboard.navigation.app_title'} />}
+                logoutLabel={<LocalizedText translationKey={'dashboard.navigation.logout_button'} />}
+            />
+
+            {/* Main content area */}
+            <main
+                className={cn(
+                    // Base styles
+                    'pt-16 p-6',
+                    // Transition to match drawer/header
+                    'transition-all duration-300 ease-[cubic-bezier(0.4,0,0.6,1)]',
+                    // Margin based on drawer state
+                    navigationDrawerOpen ? 'ml-[300px]' : 'ml-[57px]'
+                )}
+            >
+                {props.content}
+            </main>
         </>
     );
 };
